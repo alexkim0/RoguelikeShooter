2bcd97fd917f4271e4021a55bf322f3df47ba00f
Alex Kim <tlsghk2003@gmail.com>
2025-11-24 02:56:03 -0800

    Added weapon swapping system and rifle mesh

--- Assets/Scripts/GunScripts/Rifle.cs
+++ Assets/Scripts/GunScripts/Rifle.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using System.Collections;
 
 public class Rifle : HitscanGunSystem
 {
+    [Header("Rifle References")]
+    public LineRenderer laserLineGenerator;
+    public Transform laserOrigin;
+    public float laserDuration = 0.5f;
 
+    protected override void Shoot()
+    {
+
+        laserLineGenerator.SetPosition(0, laserOrigin.position);
+        base.Shoot();
+
+        if (rayHit.collider)
+        {
+            laserLineGenerator.SetPosition(1, rayHit.point);
+        } else
+        {
+            laserLineGenerator.SetPosition(1, laserOrigin.position + (fpsCam.transform.forward * range));
+        }
+        StartCoroutine(ShootLaser());
+    }
+
+    IEnumerator ShootLaser()
+    {
+        laserLineGenerator.enabled = true;
+        yield return new WaitForSeconds(laserDuration);
+        laserLineGenerator.enabled = false;
+    }
 }
