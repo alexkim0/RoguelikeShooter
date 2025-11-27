fdf897c212798356a66f27b4f556054dccf1b7d5
elvinnguyen <145058832+elvinnguyen@users.noreply.github.com>
2025-11-26 18:11:32 -0800

    added shield bar and shield text

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -8,28 +8,55 @@ using System.Collections;
 public class PlayerStats : MonoBehaviour
 {
     public float currentHealth;
+    public float currentShield;
+    public float maxShield = 100f;
     public float maxHealth = 100f;
 
     [Header("references")]
     public Image healthBar;
+    public Image shideldBar;
     public TextMeshProUGUI healthText;
+    public TextMeshProUGUI shieldText;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentHealth = maxHealth;
+        currentShield = maxShield;
     }
 
     // Update is called once per frame
     void Update()
     {
         healthBar.fillAmount = Mathf.Clamp(currentHealth / maxHealth, 0, 1);
+        shideldBar.fillAmount = Mathf.Clamp(currentShield / maxShield, 0, 1);
+
         healthText.text = $"{currentHealth} / {maxHealth}";
+        shieldText.text = $"{currentShield} / {maxShield}";
     }
 
     public void TakeDamage(float damageAmt)
     {
-        currentHealth -= damageAmt;
+        if (currentShield > 0)
+        {
+            currentShield -= damageAmt;
+
+            if (currentShield < 0)
+            {
+                currentShield = 0;
+                currentHealth += currentShield; // currentShield is negative here
+            }
+        }
+        else
+        {
+            currentHealth -= damageAmt;
+
+            if (currentHealth < 0)
+            {
+                currentHealth = 0;
+            }
+        }   
+
         //IsDead();
     }
 
