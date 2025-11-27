286ce233239a2dc447502b8d0d32113859d2082b
elvinnguyen <145058832+elvinnguyen@users.noreply.github.com>
2025-11-26 23:42:42 -0800

    increase spawn rate as time goes on

--- Assets/Scripts/MapEnemySpawner.cs
+++ Assets/Scripts/MapEnemySpawner.cs
@@ -5,6 +5,9 @@ public class MapEnemySpawner : EnemySpawner
     [Header("Spawn Settings")]
     public float spawnTimeInterval = 10f;
     private float spawnTimer = 0f;
+    float currentTime = 0f;
+    float nextIncreaseTime = 30f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,12 +18,25 @@ public class MapEnemySpawner : EnemySpawner
     // Update is called once per frame
     void Update()
     {
+        currentTime = Time.time;
         spawnTimer -= Time.deltaTime;
+
+        IncreaseSpawnedEnemies();
+
         if (spawnTimer <= 0)
         {
-            enemyCount = 1;
             SpawnEnemies();
             spawnTimer = spawnTimeInterval;
+        }
+    }
+
+    void IncreaseSpawnedEnemies()
+    {
+        if (currentTime >= nextIncreaseTime)
+        {
+            nextIncreaseTime += 30f;
+            enemyCount += 5;
+            Debug.Log("Increasing enemy count to: " + enemyCount);
         }
     }
 }
